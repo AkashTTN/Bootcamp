b2c5b8ac4af49cbdc75391fa9fe34d712e46f674
AkashTTN <akash.aggarwal@tothenew.com>
2020-04-28 09:35:42 +0530

    fixed createServer callback arguments

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var http = require("http");
-var server = http.createServer(function (request, response, error) {
-    if (error) {
-        throw new Error(error);
-    }
+var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write("<!DOCTYPE html>");
     response.write("<html>");
